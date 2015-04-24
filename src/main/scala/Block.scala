a07be766ac9a2c5c3e3b7a4c306b9df073373247
Jack Kolb <jkolb@berkeley.edu>
2015-04-24 15:19:55 -0700

    Renamed Discontinuity to Joint

--- src/main/scala/Block.scala
+++ src/main/scala/Block.scala
@@ -10,7 +10,7 @@ import lpsolve._
   * @param distance The distance from the normal vector to the center of the rock block.
   * Accessed as 'd'.
   * @param phi The friction angle (phi) of the face.
-  * @cohesion: The cohesion of the face.
+  * @param cohesion The cohesion of the face.
   */
 case class Face(normalVec: (Double,Double,Double), distance: Double,
                 val phi: Double, val cohesion: Double) {
@@ -28,20 +28,21 @@ case class Face(normalVec: (Double,Double,Double), distance: Double,
 case class Block(center: (Double,Double,Double), val faces: List[Face]) {
   val (centerX, centerY, centerZ) = center
 
-  def intersects(discontinuity: Discontinuity) = {
+  def intersects(joint: Joint): Boolean = {
     val solver = LpSolve.makeLp(0, 4) // 0 constraints (so far), 4 Variables: x, y, z, and s
     solver.setMinim()
     solver.setObjFn(Array[Double](0.0, 0.0, 0.0, 1.0)) // Minimize s
     (1 to 4).foreach {solver.setUnbounded(_)} // All variables are free
 
-    // Restrict our attention to plane of discontinuity
-    solver.addConstraint(Array[Double](discontinuity.a, discontinuity.b, discontinuity.c, 0.0),
-                         LpSolve.EQ, discontinuity.d)
+    // Restrict our attention to plane of joint
+    solver.addConstraint(Array[Double](joint.a, joint.b, joint.c, 0.0),
+                         LpSolve.EQ, joint.d)
     // Require s to be within planes defined by faces of block
-    faces.foreach {face => solver.addConstraint(Array[Double](face.a, face.b, face.c, -1.0), LpSolve.LE, face.d)}
-    // Require s to be within planes defining shape of discontinuity
-    discontinuity.globalCoordinates.foreach {case ((a,b,c),d) =>
-            solver.addConstraint(Array[Double](a, b, c, -1.0), LpSolve.LE, d)}
+    faces.foreach {face => solver.addConstraint(
+        Array[Double](face.a, face.b, face.c, -1.0), LpSolve.LE, face.d)}
+    // Require s to be within planes defining shape of joint
+    joint.globalCoordinates.foreach {case ((a,b,c),d) =>
+        solver.addConstraint(Array[Double](a, b, c, -1.0), LpSolve.LE, d)}
     solver.solve()
     val s = solver.getObjective()
     solver.deleteLp()
