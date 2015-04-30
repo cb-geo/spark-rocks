39e44ebafbf6e3b53a396bf674bea650a6d31028
Jack Kolb <jkolb@berkeley.edu>
2015-04-29 19:20:10 -0700

    Tested Block functionality.   * Added unit tests for Block intersection and redundancy checking   * Fixed revealed errors   * Fixed a mistake in the original joint definition   * Made changes to inputProcessing to enable compilation, still needs to be properly changed

--- src/main/scala/inputProcessing.scala
+++ src/main/scala/inputProcessing.scala
@@ -7,27 +7,27 @@ import scala.collection.mutable.ListBuffer
 object inputProcessor {
   // Generates face object from read inputs and adds it to the beginning of the list faces
   var transition = false
-  var shape = List.empty[(Double, Double, Double)]
+  var shape = List.empty[((Double, Double, Double), Double)]
   def addFace(normalVec: (Double, Double, Double), distance: Double,
               phi: Double, cohesion: Double, faces: ListBuffer[Face]) : Unit = {
-    val currentFace = Face(normalVec, distance, phi, cohesion) 
+    val currentFace = Face(normalVec, distance, phi, cohesion)
     faces += currentFace
   }
 
   // Generates joint object from read inputs and adds it to the beginning of the list joints
   def addJoint(normalVec: (Double, Double, Double), center: (Double, Double, Double),
                dipAngle: Double, dipDirection: Double, distance: Double, phi: Double,
-               cohesion: Double, shape: List[(Double, Double, Double)],
+               cohesion: Double, shape: List[((Double, Double, Double), Double)],
                joints: ListBuffer[Joint]) : Unit = {
-    val  currentJoint = Joint(normalVec, center, dipAngle, dipDirection, 
-                              distance, phi, cohesion, shape) 
+    val  currentJoint = Joint(normalVec, distance, center, dipAngle, dipDirection,
+                              phi, cohesion, shape)
     joints += currentJoint
   }
 
   // Processes input file: Add rock volume faces and joints to respective input list
   def readInput(fileName: String, rockVolume: ListBuffer[Face],
                 joints: ListBuffer[Joint]) : Unit = {
-    try { 
+    try {
       val inputSource = Source.fromFile(fileName) // Open input file
       for (line <- inputSource.getLines) {
         // Check if transitioning to joint input: % in input indicates end of rock volume inputs
@@ -43,7 +43,7 @@ object inputProcessor {
           addFace((a,b,c), d, phi, cohesion, rockVolume)
         } else {
           // When done with rock volume input, process joint input
-          shape = List.empty[(Double, Double, Double)]
+          shape = List.empty[((Double, Double, Double), Double)]
           val inputLine = line.split(" ").map(_.toDouble)
           val inputLength = inputLine.size
           val (a,b,c) = (inputLine(0), inputLine(1), inputLine(2))
@@ -54,8 +54,10 @@ object inputProcessor {
           val phi = inputLine(9)
           val cohesion = inputLine(10)
           if (inputLength > 11) {
+            // FIXME This needs to be updated to accommodate changes in Joint
             for (i <- 0 until (inputLength - 11)/3)  {
-              shape :::= List((inputLine(3*i+11), inputLine(3*i+12), inputLine(3*i+13)))            
+              // Last 0.0 is a placeholder that needs to be changed
+              shape :::= List(((inputLine(3*i+11), inputLine(3*i+12), inputLine(3*i+13)), 0.0))
             }
           }
           // for (i <- 11 until (inputLine.size-3)) shape :::= List((inputLine(i), inputLine(i+1), inputLine(i+2)))
