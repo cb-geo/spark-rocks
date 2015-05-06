7011582157621c0228ff3f3f2faa6a8b1f71eec4
Jack Kolb <jkolb@berkeley.edu>
2015-05-05 21:32:20 -0700

    Update LinearProgramSpec, all tests pass as expected

--- src/test/scala/LinearProgramSpec.scala
+++ src/test/scala/LinearProgramSpec.scala
@@ -9,17 +9,22 @@ class LinearProgramSpec extends FunSuite {
     val solver = new LinearProgram(1)
     solver.setObjFun(List(1.0), LinearProgram.MAX)
     solver.addConstraint(List(1.0), LinearProgram.EQ, 5.0)
+
     val (varSettings, opt) = solver.solve().get
     assert(varSettings.length == 1)
-    assert(math.abs(varSettings(0) - 5.0) <= EPSILON && math.abs(opt - 5.0) <= EPSILON)
+    assert(math.abs(varSettings(0) - 5.0) <= EPSILON)
+    assert(math.abs(opt - 5.0) <= EPSILON)
   }
 
   test("Minimizing x subject to x = 5 should produce result 5") {
     val solver = new LinearProgram(1)
     solver.setObjFun(List(1.0), LinearProgram.MIN)
     solver.addConstraint(List(1.0), LinearProgram.EQ, 5.0)
-    val solution = solver.solve().get._2
-    assert(math.abs(solution - 5.0) <= EPSILON)
+
+    val (varSettings, opt) = solver.solve().get
+    assert(varSettings.length == 1)
+    assert(math.abs(varSettings(0) - 5.0) <= EPSILON)
+    assert(math.abs(opt - 5.0) <= EPSILON)
   }
 
   test("Maximizing x + y subject to x <= 5 and y <= 4 should produce result 9") {
@@ -27,8 +32,12 @@ class LinearProgramSpec extends FunSuite {
     solver.setObjFun(List(1.0, 1.0), LinearProgram.MAX)
     solver.addConstraint(List(1.0, 0.0), LinearProgram.LE, 5.0)
     solver.addConstraint(List(0.0, 1.0), LinearProgram.LE, 4.0)
-    val solution = solver.solve().get._2
-    assert(math.abs(solution - 9.0) <= EPSILON)
+
+    val (varSettings, opt) = solver.solve().get
+    assert(varSettings.length == 2)
+    assert(math.abs(varSettings(0) - 5.0) <= EPSILON)
+    assert(math.abs(varSettings(1) - 4.0) <= EPSILON)
+    assert(math.abs(opt - 9.0) <= EPSILON)
   }
 
   test("Minimizing x + y subject to x >= 5 and y >= 4 should produce result 9") {
@@ -36,19 +45,27 @@ class LinearProgramSpec extends FunSuite {
     solver.setObjFun(List(1.0, 1.0), LinearProgram.MIN)
     solver.addConstraint(List(1.0, 0.0), LinearProgram.GE, 5.0)
     solver.addConstraint(List(0.0, 1.0), LinearProgram.GE, 4.0)
-    val solution = solver.solve().get._2
-    assert(math.abs(solution - 9.0) <= EPSILON)
+
+    val (varSettings, opt) = solver.solve().get
+    assert(varSettings.length == 2)
+    assert(math.abs(varSettings(0) - 5.0) <= EPSILON)
+    assert(math.abs(varSettings(1) - 4.0) <= EPSILON)
+    assert(math.abs(opt - 9.0) <= EPSILON)
   }
 
-  test("Minimizing x - y subject to 5 <= x <= 6, 7 <= y <= 11, should produce result -7") {
+  test("Minimizing x - y subject to 5 <= x <= 6, 7 <= y <= 11, should produce result -6") {
     val solver = new LinearProgram(2)
     solver.setObjFun(List(1.0, -1.0), LinearProgram.MIN)
     solver.addConstraint(List(1.0, 0.0), LinearProgram.LE, 6.0)
     solver.addConstraint(List(0.0, 1.0), LinearProgram.GE, 7.0)
     solver.addConstraint(List(1.0, 0.0), LinearProgram.GE, 5.0)
     solver.addConstraint(List(0.0, 1.0), LinearProgram.LE, 11.0)
-    val solution = solver.solve().get._2
-    assert(math.abs(solution + 6.0) <= EPSILON)
+
+    val (varSettings, opt) = solver.solve().get
+    assert(varSettings.length == 2)
+    assert(math.abs(varSettings(0) - 5.0) <= EPSILON)
+    assert(math.abs(varSettings(1) - 11.0) <= EPSILON)
+    assert(math.abs(opt + 6.0) <= EPSILON)
   }
 
   test("Maximizing sum of coordinates in unit cube should produce result 3") {
@@ -60,8 +77,13 @@ class LinearProgramSpec extends FunSuite {
     solver.addConstraint(List(1.0, 0.0, 0.0), LinearProgram.LE, 1.0)
     solver.addConstraint(List(0.0, 1.0, 0.0), LinearProgram.LE, 1.0)
     solver.addConstraint(List(0.0, 0.0, 1.0), LinearProgram.LE, 1.0)
-    val solution = solver.solve().get._2
-    assert(math.abs(solution - 3.0) <= EPSILON)
+
+    val (varSettings, opt) = solver.solve().get
+    assert(varSettings.length == 3)
+    assert(math.abs(varSettings(0) - 1.0) <= EPSILON)
+    assert(math.abs(varSettings(1) - 1.0) <= EPSILON)
+    assert(math.abs(varSettings(2) - 1.0) <= EPSILON)
+    assert(math.abs(opt - 3.0) <= EPSILON)
   }
 
   test("Minimum z coordinate within plane -x + z = 1 within 2-cube should be 1") {
@@ -74,7 +96,11 @@ class LinearProgramSpec extends FunSuite {
     solver.addConstraint(List(0.0, 1.0, 0.0), LinearProgram.LE, 2.0)
     solver.addConstraint(List(0.0, 0.0, 1.0), LinearProgram.LE, 2.0)
     solver.addConstraint(List(-1.0, 0.0, 1.0), LinearProgram.EQ, 1.0)
-    val solution = solver.solve().get._2
-    assert(math.abs(solution - 1.0) <= EPSILON)
+
+    val (varSettings, opt) = solver.solve().get
+    assert(varSettings.length == 3)
+    assert(math.abs(varSettings(0)) <= EPSILON)
+    assert(math.abs(varSettings(1)) <= EPSILON)
+    assert(math.abs(varSettings(2) - 1.0) <= EPSILON)
   }
 }
