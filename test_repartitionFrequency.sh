8eb23ff5e8f33878f23ed1d994228983d7981acf
mhgardner <shellshocked2003@gmail.com>
2015-05-11 02:25:14 -0700

    Testing and plotting: shell script for testing and some results plotted. Need to add weak scaling results and strong scaling currently running

--- test_repartitionFrequency.sh
+++ test_repartitionFrequency.sh
@@ -7,9 +7,9 @@ echo Cleaned blocks.json
 ./testGen.py testBlocks.txt 40000
 
 #Iterate over different partition counts
-for i in 4 8 12 18 24 36 48 96
+for i in 1 2 5 10 15 20 30 50 75 200
 do
-    spark-submit --master local[16] --driver-memory 20G target/scala-2.10/CS267-final-assembly-1.0.jar testBlocks.txt $i 15
+    spark-submit --master local[16] --driver-memory 20G target/scala-2.10/CS267-final-assembly-1.0.jar testBlocks.txt 8 $i
     rm -rf blocks.json
     echo Tested $i partitions
 done
