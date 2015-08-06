52c7dea12d895ab424ea5a9c66b28f056d5d6603
Michael Gardner <shellshocked2003@gmail.com>
2015-08-05 22:16:49 -0700

    Added method for ordering vertices counter-clockwise. Still need to address issues with generating connectivity.

--- src/main/scala/edu/berkeley/ce/rockslicing/BlocksVTK.scala
+++ src/main/scala/edu/berkeley/ce/rockslicing/BlocksVTK.scala
@@ -41,6 +41,56 @@ object BlocksVTK {
       DenseMatrix.eye[Double](3)
     }
   }
+
+    /**
+   * Finds the center of a list of vertices - defined as the average coordinate of all the vertices in the list
+   * @param vertices List of vertices
+   * @return Coordinate of center of vertices
+   */
+  private def findCenter(vertices: Seq[(Double, Double, Double)]): (Double, Double, Double) = {
+    (vertices.map(_._1).sum / vertices.length.toDouble,
+      vertices.map(_._2).sum / vertices.length.toDouble,
+      vertices.map(_._3).sum / vertices.length.toDouble)
+  }
+
+  /**
+   * Compares pointA to pointB. If pointA is first relative to pointB rotating counter-clockwise about center,
+   * the method returns true. Input vectors should have same z-coordinate - comparison is based in 2-D
+   * @param pointA Point of interest
+   * @param pointB Point of comparison
+   * @param center Reference point for comparison
+   * @return Returns TRUE if pointA is first relative to pointB, FALSE otherwise
+   */
+  private def ccwCompare(pointA: (Double, Double, Double), pointB: (Double, Double, Double),
+                           center: (Double, Double, Double)): Boolean = {
+    assert(math.abs(pointA._3 - pointB._3) < BlocksVTK.EPSILON)
+    if ((pointA._1 - center._1 < BlocksVTK.EPSILON) && (pointB._1 - center._1 >= -BlocksVTK.EPSILON)) {
+      return true
+    }
+    if ((pointA._1 - center._1 >= -BlocksVTK.EPSILON) && (pointB._1 - center._1 < BlocksVTK.EPSILON)) {
+      return false
+    }
+    if ((math.abs(pointA._1 - center._1) < BlocksVTK.EPSILON) &&
+        (math.abs(pointB._1 - center._1) < BlocksVTK.EPSILON)) {
+      if ((pointA._2 - center._2 >= BlocksVTK.EPSILON) || (pointB._2 - center._2 >= BlocksVTK.EPSILON)) {
+        return pointA._2 > pointB._2
+      }
+      return pointB._2 > pointA._2
+    }
+    // the cross product of vectors (pointA - center) and (pointB - center)
+    val det = ((pointA._1 - center._1) * (pointB._2 - center._2) -
+               (pointB._1 - center._1) * (pointA._2 - center._2)).toInt
+    if (det > 0) {
+      return true
+    }
+    if (det < 0) {
+      return false
+    }
+    // pointA and pointB are on the same line from the center, so check which one is closer to the center
+    val d1 = (pointA._1 - center._1) * (pointA._1 - center._1) + (pointA._2 - center._2) * (pointA._2 - center._2)
+    val d2 = (pointB._1 - center._1) * (pointB._1 - center._1) + (pointB._2 - center._2) * (pointB._2 - center._2)
+    return d1 > d2
+  }
 }
 
 /**
@@ -48,9 +98,7 @@ object BlocksVTK {
  */
 case class BlocksVTK(blocks: Seq[Block]) {
   val blockID = blocks.indices
-  val faceVertices = blocks map { // Sequence of maps of faces and their corresponding vertices
-    _.findVertices
-  }
+  val faceVertices = blocks map { _.findVertices } // Sequence of maps of faces and their corresponding vertices
 
   /**
    * Finds the list of unique vertices for the entire list of input faces
@@ -58,23 +106,50 @@ case class BlocksVTK(blocks: Seq[Block]) {
    * @return List of unique vertices
    */
   def vertices(faceSeq: Seq[Map[Face, Seq[(Double, Double, Double)]]]): Seq[(Double, Double, Double)] = {
-    ((faceSeq flatMap {
-      _.values
-    }) flatMap (_.distinct)).distinct.toList
+    (faceSeq flatMap { _.values }).flatten.distinct.toList
   }
 
+  /**
+   * Rotates all the vertices of all the faces in the input such that they lie in the x-y plane
+   * @param faceSeq Sequence of maps of faces and their vertices
+   * @return Sequence of maps of faces and their vertices rotated such they lie in the x-y plane
+   */
+  def rotatedVerts(faceSeq: Seq[Map[Face, Seq[(Double, Double, Double)]]]):
+  Seq[Map[Face, Seq[(Double, Double, Double)]]] = {
+    faceSeq map {
+      case face =>
+        face.keys.zip(
+          face.map { kv =>
+            val R = BlocksVTK.rotationMatrix(kv._1.normalVec, (0.0, 0.0, 1.0))
+            kv._2 map { vertex =>
+              val rotatedVert = R * DenseVector[Double](vertex._1, vertex._2, vertex._3)
+              (rotatedVert(0), rotatedVert(1), rotatedVert(2))
+            }
+          }
+        ).toMap
+    }
+  }
+
+  /**
+   * Orders vertices in a counter-clockwise orientation relative to unit normal of face
+   * @param faceSeq Sequence of maps of faces and their vertices
+   * @return Sequence of maps of faces and their vertices sorted in a counter-clockwise orientation
+   */
   def orderedVerts(faceSeq: Seq[Map[Face, Seq[(Double, Double, Double)]]]):
                             Seq[Map[Face, Seq[(Double, Double, Double)]]] = {
     faceSeq map {
       case face =>
         face.keys.zip(
           face.map { kv =>
-            kv._2 map { vertex =>
-              // CONTINUE HERE: ALSO NEED TO CHANGE THIS SO ROTATION MATRIX ISN'T RECALCULATED FOR EACH VERTEX
-              val rotatedVertex = BlocksVTK.rotationMatrix(kv._1.normalVec, (0.0, 0.0, 1.0)) * vertex
+            val center = BlocksVTK.findCenter(kv._2)
+            if (kv._1.normalVec._3 < -BlocksVTK.EPSILON) {
+              kv._2.sortWith(BlocksVTK.ccwCompare(_, _, center)).reverse
+            }
+            else {
+              kv._2.sortWith(BlocksVTK.ccwCompare(_, _, center))
             }
           }
-        )
+        ).toMap
     }
   }
 
@@ -83,13 +158,14 @@ case class BlocksVTK(blocks: Seq[Block]) {
    * @param faceSeq Sequence of maps of faces and their vertices
    * @return Sequence of maps of faces and the indices of their vertices in the global vertex list
    */
-  def connectivity(faceSeq: Seq[Map[Face, Seq[(Double, Double, Double)]]]): Seq[Map[Face, Seq[Int]]] = {
+  def connectivity(faceSeq: Seq[Map[Face, Seq[(Double, Double, Double)]]],
+                   verts: Seq[(Double, Double, Double)]): Seq[Map[Face, Seq[Int]]] = {
     faceSeq map {
       case face =>
         face.keys.zip(
           face.map { kv =>
             kv._2 map { case vertex =>
-              vertices(faceSeq).indexOf(vertex)
+              verts.indexOf(vertex)
             }
           }
         ).toMap
@@ -108,19 +184,3 @@ case class BlocksVTK(blocks: Seq[Block]) {
     }
   }
 }
-
-//  val vertices = {                                 // List of all unique vertices
-//    ((faceVertices flatMap {
-//      _.values
-//    }) flatMap (_.distinct)).distinct.toList
-//  }
-//  val connectivity = faceVertices map {
-//    case face =>
-//      face.keys.zip (
-//        face.map { kv =>
-//          kv._2 map { case vertex =>
-//            vertices.indexOf(vertex)
-//          }
-//        }
-//      ).toMap
-//  }
