ca4bf4f321378c9c768166ae753a69c336ee3e67
mhgardner <shellshocked2003@gmail.com>
2015-11-05 10:01:40 -0800

    Unit tests added for seven-sided block

--- src/test/scala/edu/berkeley/ce/rockslicing/BlockVTKSpec.scala
+++ src/test/scala/edu/berkeley/ce/rockslicing/BlockVTKSpec.scala
@@ -52,12 +52,17 @@ class BlockVTKSpec extends FunSuite {
   val face6_s7 = Face((0.0, 0.0, 1.0), 1.0, phi=0, cohesion=0)
   val face7_s7 = Face((0.707106781187, 0.707106781187, 0.0), 1.0, phi=0, cohesion=0)
   val face1Verts_s7 = List((0.0, 1.0, 0.0), (0.0, 0.0, 0.0), (0.0, 0.0, 1.0), (0.0, 1.0, 1.0))
-  val face2Verts_s7 = List((1.0, y_intercept, 1.0), (1.0, 0.0, 1.0), (1.0, 0.0, 0.0), (1.0, y_intercept, 0.0))
+  val face2Verts_s7 = List((1.0, y_intercept, 1.0), (1.0, 0.0, 1.0), (1.0, 0.0, 0.0),
+                           (1.0, y_intercept, 0.0))
   val face3Verts_s7 = List((0.0, 0.0, 1.0), (0.0, 0.0, 0.0), (1.0, 0.0, 0.0), (1.0, 0.0, 1.0))
-  val face4Verts_s7 = List((0.0, 1.0, 0.0), (0.0, 1.0, 1.0), (x_intercept, 1.0, 1.0), (x_intercept, 1.0, 0.0))
-  val face5Verts_s7 = List((1.0, y_intercept, 0.0), (1.0, 0.0, 0.0), (0.0, 0.0, 0.0), (0.0, 1.0, 0.0), (x_intercept, 1.0, 0.0))
-  val face6Verts_s7 = List((x_intercept, 1.0, 1.0), (0.0, 1.0, 1.0), (0.0, 0.0, 1.0), (1.0, 0.0, 1.0), (1.0, y_intercept, 1.0))
-  val face7Verts_s7 = List((x_intercept, 1.0, 1.0), (1.0, y_intercept, 1.0), (1.0, y_intercept, 0.0), (x_intercept, 1.0, 0.0))
+  val face4Verts_s7 = List((0.0, 1.0, 0.0), (0.0, 1.0, 1.0), (x_intercept, 1.0, 1.0),
+                           (x_intercept, 1.0, 0.0))
+  val face5Verts_s7 = List((1.0, y_intercept, 0.0), (1.0, 0.0, 0.0), (0.0, 0.0, 0.0), (0.0, 1.0, 0.0),
+                           (x_intercept, 1.0, 0.0))
+  val face6Verts_s7 = List((x_intercept, 1.0, 1.0), (0.0, 1.0, 1.0), (0.0, 0.0, 1.0), (1.0, 0.0, 1.0),
+                           (1.0, y_intercept, 1.0))
+  val face7Verts_s7 = List((x_intercept, 1.0, 1.0), (1.0, y_intercept, 1.0), (1.0, y_intercept, 0.0),
+                           (x_intercept, 1.0, 0.0))
 
   private def doubleListElementDiff(list1: Seq[Double], list2: Seq[Double]): Seq[Double] = {
     assert(list1.length == list2.length)
@@ -109,7 +114,6 @@ class BlockVTKSpec extends FunSuite {
                                 (1.0, y_intercept, 0.0), (x_intercept, 1.0, 0.0),
                                 (0.0, 1.0, 1.0), (0.0, 0.0, 1.0), (1.0, 0.0, 1.0),
                                 (0.0, 1.0, 0.0), (0.0, 0.0, 0.0), (1.0, 0.0, 0.0))
-    println(vtkBlock.tupleVertices)
     assert(expectedVertices == vtkBlock.tupleVertices)
   }
 
@@ -158,18 +162,16 @@ class BlockVTKSpec extends FunSuite {
     assert(faceConnectivities == vtkBlock.connectivityMap)
   }
 
-  // CONTINUE HERE
   test("Connectivities for each face should be in terms of vertex ID's (seven-sided block)") {
     val vtkBlock = BlockVTK(sevenSidedBlock)
     val faceConnectivities = Map(
-      //fix these
-      face1_s7 -> List[Int](4, 5, 1, 0),
-      face2_s7 -> List[Int](3, 2, 6, 7),
-      face3_s7 -> List[Int](1, 5, 6, 2),
-      face4_s7 -> List[Int](4, 0, 3, 7),
-      face5_s7 -> List[Int](7, 6, 5, 4),
-      face6_s7 -> List[Int](0, 1, 2, 3),
-      face7_s7 -> List[Int](//Put some number is here)
+      face1_s7 -> List[Int](7, 8, 5, 4),
+      face2_s7 -> List[Int](1, 6, 9, 2),
+      face3_s7 -> List[Int](5, 8, 9, 6),
+      face4_s7 -> List[Int](7, 4, 0, 3),
+      face5_s7 -> List[Int](2, 9, 8, 7, 3),
+      face6_s7 -> List[Int](0, 4, 5, 6, 1),
+      face7_s7 -> List[Int](0, 1, 2, 3)
     )
     assert(faceConnectivities == vtkBlock.connectivityMap)
   }
@@ -182,14 +184,36 @@ class BlockVTKSpec extends FunSuite {
     assert(connectionsDiff.filter(_ > NumericUtils.EPSILON).isEmpty)
   }
 
+  test("Connectivity map should flatten into list of integers (seven-sided block)") {
+    val vtkBlock = BlockVTK(sevenSidedBlock)
+    val faceConnectivities = Seq[Int](0, 1, 2, 3, 0, 4, 5, 6, 1, 7, 8, 5, 4,
+                                      1, 6, 9, 2, 7, 4, 0, 3, 2, 9, 8, 7, 3,
+                                      5, 8, 9, 6)
+    val connectionsDiff = intListElementDiff(faceConnectivities.toSeq, vtkBlock.connectivity.toSeq)
+    assert(connectionsDiff.filter(_ > NumericUtils.EPSILON).isEmpty)
+  }
+
   test("Number of faces should be 6") {
     val vtkBlock = BlockVTK(unitCube)
     assert(vtkBlock.faceCount == 6)
   }
 
+  test("Number of faces should be 7 (seven-sided block)") {
+    val vtkBlock = BlockVTK(sevenSidedBlock)
+    assert(vtkBlock.faceCount == 7)
+  }
+
   test("Offsets should be incremented by number of vertices on each face when iterating through list of faces") {
     val vtkBlock = BlockVTK(unitCube)
     val expectedOffsets = List(4, 8, 12, 16, 20, 24)
+    val offsetDiff = intListElementDiff(expectedOffsets, vtkBlock.offsets)
+    assert(offsetDiff.filter(_ > NumericUtils.EPSILON).isEmpty)
+  }
+
+  test("Offsets should be incremented by number of vertices on each face when iterating through list of faces "+
+       "(seven-sided block)") {
+    val vtkBlock = BlockVTK(sevenSidedBlock)
+    val expectedOffsets = List(4, 9, 13, 17, 21, 26, 30)
     val offsetDiff = intListElementDiff(expectedOffsets, vtkBlock.offsets)
     assert(offsetDiff.filter(_ > NumericUtils.EPSILON).isEmpty)
   }
@@ -203,6 +227,22 @@ class BlockVTKSpec extends FunSuite {
       face4.a, face4.b, face4.c,
       face5.a, face5.b, face5.c,
       face3.a, face3.b, face3.c
+    ) 
+    val normals = vtkBlock.normals
+    val normalsDiff = doubleListElementDiff(expectedNormals, normals)
+    assert(normalsDiff.filter(_ > NumericUtils.EPSILON).isEmpty)
+  }
+
+  test("Normals tuples should flatten into list of integers (seven-sided block)") {
+    val vtkBlock = BlockVTK(sevenSidedBlock)
+    val expectedNormals = Seq[Double](
+      face7_s7.a, face7_s7.b, face7_s7.c,
+      face6_s7.a, face6_s7.b, face6_s7.c,
+      face1_s7.a, face1_s7.b, face1_s7.c,
+      face2_s7.a, face2_s7.b, face2_s7.c,
+      face4_s7.a, face4_s7.b, face4_s7.c,
+      face5_s7.a, face5_s7.b, face5_s7.c,
+      face3_s7.a, face3_s7.b, face3_s7.c
     ) 
     val normals = vtkBlock.normals
     val normalsDiff = doubleListElementDiff(expectedNormals, normals)
