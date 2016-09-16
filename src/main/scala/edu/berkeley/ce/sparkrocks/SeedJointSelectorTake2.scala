636c9925f11edc9fc89736da1eae7f50991d5a6a
Michael Gardner <shellshocked2003@gmail.com>
2016-09-16 10:00:24 -0700

    Latest progress on updated load balance - almost done

--- src/main/scala/edu/berkeley/ce/sparkrocks/SeedJointSelectorTake2.scala
+++ src/main/scala/edu/berkeley/ce/sparkrocks/SeedJointSelectorTake2.scala
@@ -9,40 +9,73 @@ import scala.annotation.tailrec
 object SeedJointSelectorTake2 {
 
   @tailrec
-  def generateSeedBlocks(jointSets: Seq[Seq[Joint]], inputVolume: Seq[Block], numPartitions: Int,
-                         jointSetSpan: Int = 1): (Seq[Block], Seq[Joint]) = {
+  def generateSeedBlocks(jointSets: Seq[Seq[Joint]], inputVolume: Block, numPartitions: Int,
+                         jointSetSpan: Int = 1, seedBlocks: Seq[Block] = Seq.empty[Block],
+                         remainingJoints: Seq[Joint] = Seq.empty[Joint]): (Seq[Block], Seq[Joint]) = {
     if (jointSetSpan > jointSets.length) {
-      throw new IllegalStateException("ERROR: Unable to find satisfactory seed joints")
+      if (seedBlocks.isEmpty) {
+        throw new IllegalStateException("ERROR: Unable to find satisfactory seed joints")
+      } else {
+        println(s"""WARNING: Unable to find at least desired number of partitions. Continuing run with
+        ${seedBlocks.length} partitions.""")
+        (seedBlocks, remainingJoints)
+      }
     }
     // Find joints that cut input volume into approximately equal pieces
-    val (seedJoints, remainingJoints) = findSeedJoints(jointSets, inputVolume, numPartitions, jointSetSpan)
-    // Unable to find sufficient number of seed joints
-    val seedBlocks = seedJoints.foldLeft(inputVolume) { (currentBlocks, joint) =>
-      currentBlocks.flatMap(_.cut(joint))
-    }
+    val (partitionBlocks, leftOverJoints) = findSeedBlocks(jointSets, inputVolume, numPartitions, jointSetSpan)
     // Sufficient partitions found
-    if (seedBlocks.length >= numPartitions) {
-      (seedBlocks, remainingJoints)
+    if (partitionBlocks.length >= numPartitions) {
+      (partitionBlocks, leftOverJoints)
     // Unable to find sufficient partitions, run with greater span
     } else {
-      generateSeedBlocks(jointSets, inputVolume, numPartitions, jointSetSpan + 1)
+      generateSeedBlocks(jointSets, inputVolume, numPartitions, jointSetSpan + 1, partitionBlocks, leftOverJoints)
     }
   }
 
   @tailrec
-  def findSeedJoints(jointSets: Seq[Seq[Joint]], inputVolume: Seq[Block], numPartitions: Int, jointSetSpan: Int,
-                     seedJoints: Seq[Joint] = Seq.empty[Joint], remainingJoints: Seq[Joint] = Seq.empty[Joint]):
-  (Seq[Joint], Seq[Joint]) = {
-    // All joint sets checked, return
-    if (jointSets.isEmpty) {
-      (seedJoints, remainingJoints)
+  def findSeedBlocks(jointSets: Seq[Seq[Joint]], inputVolume: Block, numPartitions: Int, jointSetSpan: Int,
+                     seedBlocks: Seq[Block] = Seq.empty[Block], remainingJoints: Seq[Joint] = Seq.empty[Joint]):
+  (Seq[Block], Seq[Joint]) = {
+    // All joint sets checked or span too large
+    if (jointSets.isEmpty || jointSetSpan > jointSets.length) {
+      (seedBlocks, remainingJoints)
     }
     // Check that joint set is persistent
     if (jointSets.head(0).shape.isEmpty) {
-      val selectedJoints = searchJointSets(jointSets.head, inputVolume, )
+      val (seedJoints, leftOverJoints) = searchJointSets(jointSets, inputVolume, numPartitions, jointSetSpan)
+      val partitioning = seedJoints.foldLeft(Seq(inputVolume)) { (currentBlocks, joint) =>
+        currentBlocks.flatMap(_.cut(joint))
+      }
+      // Insufficient partitions found
+      if (partitioning.length < numPartitions) {
+        findSeedBlocks(jointSets.tail, inputVolume, numPartitions, jointSetSpan, partitioning,
+          remainingJoints ++ jointSets.head)
+      // Sufficient partitions, return
+      } else {
+        (partitioning, remainingJoints ++ leftOverJoints)
+      }
     // Joint set non-persistent, check next one
     } else {
-      findSeedJoints(jointSets.tail, inputVolume, numPartitions, jointSetSpan, seedJoints, remainingJoints)
+      findSeedBlocks(jointSets.tail, inputVolume, numPartitions, jointSetSpan, seedBlocks, remainingJoints)
     }
+  }
+
+  def searchJointSets(jointSets: Seq[Seq[Joint]], inputVolume: Block, numPartitions: Int, jointSetSpan: Int,
+                      spanCount: Int = 1, seedJoints: Seq[Joint] = Seq.empty[Joint],
+                      remainingJoints: Seq[Joint] = Seq.empty[Joint]): (Seq[Joint], Seq[Joint]) = {
+    // Checked specified span of joint sets, return
+    if (spanCount > jointSetSpan) {
+      (seedJoints, remainingJoints)
+    } else {
+      val newSeedJoints = findSeedJoints(jointSets.head, inputVolume, numPartitions,
+        spanCount, jointSetSpan)
+      val remainingFromJointSet = jointSets.head.diff(newSeedJoints)
+      searchJointSets(jointSets.tail, inputVolume, numPartitions, jointSetSpan, spanCount + 1,
+        seedJoints ++ newSeedJoints, remainingJoints ++ remainingFromJointSet)
+    }
+  }
+
+  def findSeedJoints(): Seq[Joint] = {
+
   }
 }
